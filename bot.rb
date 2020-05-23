e5ee99a937c9610f358b45e6dd4c0a9ade90d5db
Alx19 <alx19sucharev@gmail.com>
2020-05-23 12:47:25 +0300

    bot logic

---
+++ bot.rb
@@ -0,0 +1,18 @@
+require_relative 'config.rb'
+require 'telegram/bot'
+
+Telegram::Bot::Client.run(TOKEN) do |bot|
+  bot.listen do |message|
+    case message.text
+    when '/pibor@SubllmeBot'
+      PHRASES.sample.each do |phrase|
+        bot.api.send_message(chat_id: message.chat.id, text: phrase)
+        sleep(1)
+      end
+    when '/pidoreg@SubllmeBot'
+      bot.api.send_message(chat_id: message.chat.id, text: 'Эй, ты уже в игре!')
+    when '/pidorall@SubllmeBot'
+      bot.api.send_message(chat_id: message.chat.id, text: "Топ-10 Пидоров за все время:\n1.nanoanna - ∞ раз")
+    end
+  end
+end
